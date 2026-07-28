c42ea1215727f54ca483bcc4afd42ca5f4983549
Maharshi-bloop <bloop.maharshi@gmail.com>
2026-07-28 17:16:26 +0530

    technology page finish

--- ART/assets/js/pages/home.js
+++ ART/assets/js/pages/home.js
@@ -1,33 +1,38 @@
 /* gallery Swiper js start */
-    new Swiper('.clientSwiper .swiper', {
-        loop: true,
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-        slidesPerView: 2,
-        paginationClickable: true,
-        speed: 1000,
-        /* autoplay: {
-            delay: 0,
-            disableOnInteraction: false,
-        }, */
-        spaceBetween: 20,
-        breakpoints: {
-            1920: {
-                slidesPerView: 2,
-                spaceBetween: 30
-            },
-            1366: {
-                slidesPerView: 2,
-                spaceBetween: 30
-            },
-            480: {
-                slidesPerView: 1.5,
-                spaceBetween: 10
-            },
-            320: {
-                slidesPerView: 1.5,
-                spaceBetween: 10
-            }
+new Swiper('.clientSwiper .swiper', {
+    loop: true,
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    slidesPerView: 2,
+    paginationClickable: true,
+    speed: 1000,
+    /* autoplay: {
+        delay: 0,
+        disableOnInteraction: false,
+    }, */
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+        dragSize: 40,
+    },
+    spaceBetween: 20,
+    breakpoints: {
+        1920: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+        1366: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+        480: {
+            slidesPerView: 1.5,
+            spaceBetween: 10
+        },
+        320: {
+            slidesPerView: 1.5,
+            spaceBetween: 10
         }
-    });
-    /* gallery Swiper js end */
+    }
+});
+/* gallery Swiper js end */
