b8461ec93f81cf0b2e430cbb115ebd72c4efec9d
Maharshi-bloop <bloop.maharshi@gmail.com>
2026-08-07 13:03:31 +0530

    product popup and resource page

--- ART/assets/js/pages/template.js
+++ ART/assets/js/pages/template.js
@@ -1,4 +1,4 @@
-$(".accordianBody").hide().first().show();
+/* $(".accordianBody").hide().first().show();
 $(".accordianList").first().addClass("active");
 
 $(".accordianHeading").on("click", function () {
@@ -10,4 +10,28 @@ $(".accordianHeading").on("click", function () {
 
     item.toggleClass("active");
     body.stop(true, true).slideToggle("slow");
+}); */
+
+$(".accordianWrapper").each(function () {
+    const $wrapper = $(this);
+
+    $wrapper.find(".accordianBody").hide();
+    $wrapper.find(".accordianList").removeClass("active");
+
+    $wrapper.find(".accordianList").first().addClass("active");
+    $wrapper.find(".accordianBody").first().show();
+});
+
+$(".accordianHeading").on("click", function () {
+
+    const $wrapper = $(this).closest(".accordianWrapper");
+    const $item = $(this).closest(".accordianList");
+    const $body = $item.find(".accordianBody");
+
+    $wrapper.find(".accordianList").not($item).removeClass("active");
+    $wrapper.find(".accordianBody").not($body).slideUp("slow");
+
+    $item.toggleClass("active");
+    $body.stop(true, true).slideToggle("slow");
+
 });
