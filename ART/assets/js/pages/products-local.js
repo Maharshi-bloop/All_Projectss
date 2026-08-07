621be1a56d0967c648feaa6165b0f9208e68a143
Maharshi-bloop <bloop.maharshi@gmail.com>
2026-08-07 17:21:17 +0530

    resource page

---
+++ ART/assets/js/pages/products-local.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+    // Open Popup
+    $(".systemsList").on("click", function (e) {
+        e.preventDefault();
+
+        let productId = $(this).attr("id");
+
+        // Ignore if no id is present
+        if (!productId) return;
+
+        $(".popUpWrapperOuter")
+            .attr("data-attr", productId)
+            .addClass("activePopup");
+    });
+
+    // Close popup on close icon
+    $(".popUpClose").on("click", function (e) {
+        e.stopPropagation();
+        $(".popUpWrapperOuter").removeClass("activePopup");
+    });
+
+    // Close popup when clicking outside popup
+    $(".popUpWrapperOuter").on("click", function (e) {
+        if ($(e.target).closest(".popUpWrapper").length === 0) {
+            $(this).removeClass("activePopup");
+        }
+    });
+
+});
+
--- ART/assets/js/pages/products.js
+++ ART/assets/js/pages/products.js
@@ -1,30 +1,40 @@
 $(document).ready(function () {
 
+
     // Open Popup
     $(".systemsList").on("click", function (e) {
         e.preventDefault();
-
         let productId = $(this).attr("id");
-
         // Ignore if no id is present
         if (!productId) return;
-
-        $(".popUpWrapperOuter")
-            .attr("data-attr", productId)
+        // Remove active from all popup
+        $(".popUpWrapperOuter").removeClass("activePopup");
+        
+        // Open only matching popup
+        $('.popUpWrapperOuter[data-attr="' + productId + '"]')
             .addClass("activePopup");
     });
 
     // Close popup on close icon
     $(".popUpClose").on("click", function (e) {
         e.stopPropagation();
-        $(".popUpWrapperOuter").removeClass("activePopup");
+        $(this)
+            .closest(".popUpWrapperOuter")
+            .removeClass("activePopup");
     });
+
+
 
     // Close popup when clicking outside popup
     $(".popUpWrapperOuter").on("click", function (e) {
+
         if ($(e.target).closest(".popUpWrapper").length === 0) {
+
             $(this).removeClass("activePopup");
+
         }
+
     });
+
 
 });
