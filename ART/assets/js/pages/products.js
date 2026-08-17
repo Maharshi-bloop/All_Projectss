3ed0c55a66d35c5c86043b7a7878e30d17660c8c
Maharshi-bloop <bloop.maharshi@gmail.com>
2026-08-17 17:20:28 +0530

    ART Last

--- ART/assets/js/pages/products.js
+++ ART/assets/js/pages/products.js
@@ -1,43 +1,37 @@
 $(document).ready(function () {
 
-
     // Open Popup
     $(".systemsList").on("click", function (e) {
-
         e.preventDefault();
 
-
         let productId = $(this).attr("id");
-
 
         // Ignore if no id is present
         if (!productId) return;
 
-
         // Remove active from all popup
         $(".popUpWrapperOuter").removeClass("activePopup");
 
-
-        // Open only matching popup
+        // Open matching popup
         $('.popUpWrapperOuter[data-attr="' + productId + '"]')
             .addClass("activePopup");
 
-
+        // Disable body scroll
+        $("body").addClass("scrollOff");
     });
-
 
 
     // Close popup on close icon
     $(".popUpClose").on("click", function (e) {
-
         e.stopPropagation();
 
         $(this)
             .closest(".popUpWrapperOuter")
             .removeClass("activePopup");
 
+        // Enable body scroll
+        $("body").removeClass("scrollOff");
     });
-
 
 
     // Close popup when clicking outside popup
@@ -47,9 +41,9 @@ $(document).ready(function () {
 
             $(this).removeClass("activePopup");
 
+            // Enable body scroll
+            $("body").removeClass("scrollOff");
         }
-
     });
-
 
 });
